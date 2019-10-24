692e3376e455bb227b221a382976b830f4654b01
cecileanne <cecileannesison@gmail.com>
2019-10-23 21:29:45 -0500

    removed materialize requirement - using cdn connection instead

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const materialize = require("materialize-css");
 
 // Sets up the Express App
 // =============================================================
