22768bf5c201f32986200b7c9e86f260155c4b29
cecileanne <cecileannesison@gmail.com>
2019-10-26 14:21:58 -0500

    sorted the array of score/friend objects to determine the best match

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -36,37 +36,57 @@ module.exports = function(app) {
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
     // console.log(newFriend);
-    friends.push(newFriend);
-    // res.json(newFriend);
     // ------------------------------------------------------------------
 
     // Determine the user's most compatible friend using the following as a guide:
     // console.log(newFriend.scores);
     // Declare besty (it will be the match)
     let besty;
+    let diffArray = [];
     // Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
     friends.forEach(friend => {
       let diff = 0;
       friend.scores.forEach((score, index) => {
         diff += Math.abs(score - newFriend.scores[index]);
-      });
-      // let diffArray = [];
-      // const friendScoreDiff = {`scoreDiff: ${diff}, friendName: ${friend.name}`}
-      // diffArray.push(friendScoreDiff);
-      //   let diffArray = [diff, friend.name];
+      }); // closes friendscore for each
+      let sortedFriend = friend.name;
+      let diffObject = { diff, sortedFriend };
+      diffArray.push(diffObject);
       //   console.log(diffArray);
-      //   sortedArray.push(diffArray)
-      //   besty = sortedArray[0];
-      //   console.log(`your best match is ${besty}`);
-
-      // ------------------------------------------------------------------
-      // SORT AND THEN IF PERSON's diff is smaller, they become the besty
-      //best match will be index 0 of sortedFriends
-      // console.log(besty)
     }); // closes for each
+
+    // ------------------------------------------------------------------
+    // SORT
+    let sortedArray = diffArray.sort(function(a, b) {
+      return a.diff - b.diff;
+    });
+    // console.log(sortedArray);
+    //best match will be index 0 of sortedFriends
+    // console.log(besty)
+    besty = sortedArray[0].sortedFriend;
+    bestyRoute = besty.replace(/\s+/g, "").toLowerCase();
+    console.log(`your best match is ${besty}`);
+    console.log(`bestyRoute is ${bestyRoute}`);
+
+    // now that the person has a besty, add them to the list of people in friends
+    friends.push(newFriend);
+    // res.json(newFriend);
 
     // TO DO Once you've found the current user's most compatible friend, display the result as a modal pop-up.
     // TO DO The modal should display both the besty.name and besty.photo of the closest match.
+    modalBesty();
+    function modalBesty() {
+      app.get(`/api/friends/${bestyRoute}`, function(req, res) {
+        const bestyGet = req.params.friend;
+        for (var friend = 0; friend < friends.length; friend++) {
+          if (bestyGet === friends[friend].routeName) {
+            return res.json(friends[friend]);
+          }
+        }
+        return res.json(false);
+      }); // closes get for besty
+      console.log(bestyGet.name, bestyGet.photo);
+    }
 
     //   app.post("#modal1", function(req, res) {
     //   }
