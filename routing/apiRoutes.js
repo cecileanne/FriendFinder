35904318dbd9221985c2b8b1a52bcc7c6817db2a
cecileanne <cecileannesison@gmail.com>
2019-10-25 21:46:25 -0500

    added in some placeholders for modal pop up

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,8 +1,13 @@
+// LOAD DATA
 const friends = require("../data/friends");
 
-// 4. Your `apiRoutes.js` file should contain two routes:
 module.exports = function(app) {
-  //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
@@ -19,41 +24,51 @@ module.exports = function(app) {
     return res.json(false);
   });
 
-  //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  // POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
   // Create New Friend - takes in JSON input
   app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     let newFriend = req.body;
-    console.log(newFriend);
+    // console.log(newFriend);
     // Using a RegEx Pattern to remove spaces from newFriend
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
     // console.log(newFriend);
     friends.push(newFriend);
     // res.json(newFriend);
-    // 6. Determine the user's most compatible friend using the following as a guide:
-    console.log(newFriend.scores);
+    // ------------------------------------------------------------------
+
+    // Determine the user's most compatible friend using the following as a guide:
+    // console.log(newFriend.scores);
+    // Declare besty (it will be the match)
     let besty;
-    //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+    // Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
     friends.forEach(friend => {
       let diff = 0;
       friend.scores.forEach((score, index) => {
         diff += Math.abs(score - newFriend.scores[index]);
       });
-      console.log(friend.name, diff);
-    });
-    //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-    //      * Example:
-    //        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-    //        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-    //        * Total Difference: **2 + 1 + 2 =** **_5_**
-    //    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-    //    * The closest match will be the user with the least amount of difference.
+      // let diffArray = [];
+      // const friendScoreDiff = {`scoreDiff: ${diff}, friendName: ${friend.name}`}
+      // diffArray.push(friendScoreDiff);
+      //   let diffArray = [diff, friend.name];
+      //   console.log(diffArray);
+      //   sortedArray.push(diffArray)
+      //   besty = sortedArray[0];
+      //   console.log(`your best match is ${besty}`);
 
-    // 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-    //    * The modal should display both the name and picture of the closest match.
-  });
-};
-// module.exports = apiRoutes;
+      // ------------------------------------------------------------------
+      // SORT AND THEN IF PERSON's diff is smaller, they become the besty
+      //best match will be index 0 of sortedFriends
+      // console.log(besty)
+    }); // closes for each
+
+    // TO DO Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+    // TO DO The modal should display both the besty.name and besty.photo of the closest match.
+
+    //   app.post("#modal1", function(req, res) {
+    //   }
+  }); // closes post
+}; // closes function(app)
